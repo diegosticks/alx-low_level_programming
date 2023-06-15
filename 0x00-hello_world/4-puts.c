456ddb9f48c98c114be53358863fb11350444ce2
Chijioke Mekowulu <c.mekowulu@yahoo.com>
2023-06-15 11:39:22 -0700

    A C program that prints an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
- \tputs("\"Programming is like building a multilingual puzzle");
- \treturn(0);
+ 	 puts("\"Programming is like building a multilingual puzzle");
+ 	 return(0);
 }
