c74ef7282d17d5907ac64e1491ab4d4575145c20
Chijioke Mekowulu <c.mekowulu@yahoo.com>
2023-06-15 11:41:37 -0700

    A C program that prints an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <studio.h>
 
-/**
-* main - prints quote
-*
-* Return - zero on success
-*
-*/
-
 int main(void)
 {
- 	 puts("\"Programming is like building a multilingual puzzle");
- 	 return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
