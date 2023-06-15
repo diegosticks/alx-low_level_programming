3edfc41005b2fa76eb78cb03ea84dac629f9bb07
Chijioke Mekowulu <c.mekowulu@yahoo.com>
2023-06-15 14:03:27 -0700

    UNIX simple operating sytem

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints size of data types
+ * Description: prints sizes of data types
+ * Return: return 0
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+
+
+	write(2, str1, 59);
+	return (1);
+}
